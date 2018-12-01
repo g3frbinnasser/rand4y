291c2174580ce37d355fc6688b04fd1ec03ded4e
randycod <45352823+randycod@users.noreply.github.com>
2018-12-02 00:29:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ client.on('message', message => {
  
     let room = message.content.split(" ").slice(1);
     let findroom = message.guild.channels.find('name', `${room}`)
-    if(message.content.startsWith(prefix + "setWelcomer")) {
+    if(message.content.startsWith(prefix + "!setWelcomer")) {
         if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
         if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
 if(!room) return message.channel.send('Please Type The Channel Name')
@@ -129,7 +129,7 @@ client.on("guildMemberAdd", member => {
     let memberavatar = member.user.avatarURL
       if (!welcomer) return;
       if(welcomer) {
-         moment.locale('ar-ly');
+         if.locale('ar-ly');
          var h = member.user;
         let heroo = new Discord.RichEmbed()
         .setColor('RANDOM')
